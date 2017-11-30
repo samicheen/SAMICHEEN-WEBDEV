b017b2a286c2f96f4429b56420bdd73f86f3a87a
Samicheen Khariwal <khariwal.s@husky.neu.edu>
2017-11-30 14:14:06 -0500

    Changed the folder path

--- utilities/filelist.js
+++ utilities/filelist.js
@@ -2,7 +2,7 @@ const app = require('../express');
 const fs = require('fs');
 app.use(function (req, res, next) {
     if(req.url.slice(-1) === '/') {
-        const folder = __dirname+'/public'+req.url;
+        const folder = __dirname+'/../public'+req.url;
         const secondToLastSlash = req.url.slice(0, -1).indexOf('/');
         const parent = folder.substring(0, secondToLastSlash);
         fs.readdir(folder, function (err, files) {
